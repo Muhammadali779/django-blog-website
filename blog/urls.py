a11be7baace4d23454d1e99759f33a494936db61
Muhammadali <baxtiyorovich779@gmail.com>
2026-01-13 22:02:24 +0500

    Update / Delete post created

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 
 from .views import (
-    HomeView,
-    BlogsView,
-    BlogDetailView,
     BlogCreateView,
+    BlogDeleteView,
+    BlogDetailView,
+    BlogsView,
+    BlogUpdateView,
+    HomeView,
 )
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('blogs/', BlogsView.as_view(), name='blogs'),
-    path('blogs/<slug:slug>', BlogDetailView.as_view(), name='blog_detail'),
-    path('blogs/create/', BlogCreateView.as_view(), name='blog_create'),
+    path("", HomeView.as_view(), name="home"),
+    path("blogs/", BlogsView.as_view(), name="blogs"),
+    path("blogs/create/", BlogCreateView.as_view(), name="blog_create"),
+    path("blogs/<slug:slug>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("delete/", BlogDeleteView.as_view(), name="blog_delete_list"),
+    path("delete/<int:id>/", BlogDeleteView.as_view(), name="blog_delete"),
+    path("blogs/update/<int:id>/", BlogUpdateView.as_view(), name="blog_update"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,87 +1,122 @@
 from datetime import datetime
 
-from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from django.views import View
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.views import View
 
 posts: list[dict] = [
     {
-        'id': 1,
-        'title': 'Erondagi namoyishlar: hozirgacha nimalar ma’lum?',
-        'slug': 'erondagi-namoyishlar-hozirgacha-nimalar-malum',
-        'published_date': datetime(2026, 1, 10, 7, 32),
-        'views': 45927,
-        'reading_minute': 8,
-        'content': '''Guvohlar tomonidan olingan videolarda rasmiylarning namoyishlarni bostirishdagi harakatlari yanada shafqatsiz tus olayotganini ko‘rsatmoqda.
+        "id": 1,
+        "title": "Erondagi namoyishlar: hozirgacha nimalar ma’lum?",
+        "slug": "erondagi-namoyishlar-hozirgacha-nimalar-malum",
+        "published_date": datetime(2026, 1, 10, 7, 32),
+        "views": 45927,
+        "reading_minute": 8,
+        "content": """Guvohlar tomonidan olingan videolarda rasmiylarning namoyishlarni bostirishdagi harakatlari yanada shafqatsiz tus olayotganini ko‘rsatmoqda.
 
                     Xalqaro axborot agentliklari voqealarni Eron hududida turib yoritish imkoniga ega emas. Bundan tashqari, mamlakat rasmiylari payshanba kuni internetni to‘liq o‘chirib qo‘ygan va bu ma’lumot yig‘ish va tekshirishni yanada qiyinlashtirgan.
 
                     Shunga qaramay, OAV ixtiyoriga mamlakatdan ayrim videoyozuvlar yetib kelmoqda, jurnalistlar Eronda bo‘lib turgan va voqealarni to‘g‘ridan to‘g‘ri kuzatish imkoniga ega bo‘lgan odamlar bilan bog‘lanmoqda.
 
                     Shanba kuni olingan videolardan birida namoyishchilar Tehronning Gisha tumanidagi ko‘chalarni nazorat qilayotgani aks etgan. Ayrim videoroliklarda esa Eronning ikkinchi eng yirik shahri bo‘lmish Mashhadda namoyishchilar xavfsizlik kuchlari bilan to‘qnashganini ko‘rish mumkin.
-        ''',
-        'tg_link': 'https://t.me/c/2828107333/11/1352'
+        """,
+        "tg_link": "https://t.me/c/2828107333/11/1352",
     },
     {
-        'id': 2,
-        'title': 'Erondagi namoyishlar: xaritalar ustida sharh',
-        'slug': 'erondagi-namoyishlar-xaritalar-ustida-sharh',
-        'published_date': datetime(2026, 1, 11, 10, 2),
-        'views': 31927,
-        'reading_minute': 10,
-        'content': '''Hozirda 31 ta viloyatdan 30 tasida namoyishlar kuzatilmoqda. Ular orasida ayniqsa Mashhad shahri alohida e’tiborga loyiq. Mashhad Tehrondan keyin aholi soni bo‘yicha ikkinchi eng yirik shahar bo‘lib, mamlakatning muhim iqtisodiy salohiyatini ta’minlaydi. Eron oliy rahbari Xominaiy aynan Mashhadda tug‘ilgan. Shahar Eron Islom Respublikasining ma’naviy tayanchi hisoblanadi, chunki bu yerda Imom Rizo majmuasi va ko‘plab diniy markazlar joylashgan. So‘nggi kunlarda ijtimoiy tarmoqlarda Mashhadda 30 metrli Eron bayrog‘i yirtib tashlangani va oyatullohga qarshi keskin shiorlar yangragani aks etgan videolar tarqaldi. Bu holat Eron rejimidan nafaqat dunyoviy qatlam, balki diniy va konservativ guruhlar ham norozilik bildirayotganini ko‘rsatadi.''',
-        'tg_link': 'https://t.me/c/2828107333/11/1329'
+        "id": 2,
+        "title": "Erondagi namoyishlar: xaritalar ustida sharh",
+        "slug": "erondagi-namoyishlar-xaritalar-ustida-sharh",
+        "published_date": datetime(2026, 1, 11, 10, 2),
+        "views": 31927,
+        "reading_minute": 10,
+        "content": """Hozirda 31 ta viloyatdan 30 tasida namoyishlar kuzatilmoqda. Ular orasida ayniqsa Mashhad shahri alohida e’tiborga loyiq. Mashhad Tehrondan keyin aholi soni bo‘yicha ikkinchi eng yirik shahar bo‘lib, mamlakatning muhim iqtisodiy salohiyatini ta’minlaydi. Eron oliy rahbari Xominaiy aynan Mashhadda tug‘ilgan. Shahar Eron Islom Respublikasining ma’naviy tayanchi hisoblanadi, chunki bu yerda Imom Rizo majmuasi va ko‘plab diniy markazlar joylashgan. So‘nggi kunlarda ijtimoiy tarmoqlarda Mashhadda 30 metrli Eron bayrog‘i yirtib tashlangani va oyatullohga qarshi keskin shiorlar yangragani aks etgan videolar tarqaldi. Bu holat Eron rejimidan nafaqat dunyoviy qatlam, balki diniy va konservativ guruhlar ham norozilik bildirayotganini ko‘rsatadi.""",
+        "tg_link": "https://t.me/c/2828107333/11/1329",
     },
 ]
 
 
 class HomeView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, 'home.html', {'posts': posts[-2:]})
+        return render(request, "home.html", {"posts": posts[-2:]})
 
 
 class BlogsView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
         query_params = request.GET
-        if query_params.get('search'):
-            search = query_params['search']
+        if query_params.get("search"):
+            search = query_params["search"]
 
             result = []
             for post in posts:
-                if search.lower() in post['title'].lower():
+                if search.lower() in post["title"].lower():
                     result.append(post)
 
-            return render(request, 'blog.html', {'posts': result, 'search': search})
+            return render(request, "blog.html", {"posts": result, "search": search})
 
-        return render(request, 'blog.html', {'posts': posts[::-1]})
+        return render(request, "blog.html", {"posts": posts[::-1]})
 
 
 class BlogDetailView(View):
     def get(self, request: HttpRequest, slug: str) -> HttpResponse:
         for post in posts:
-            if post['slug'] == slug:
-                post['views'] += 1
-                return render(request, 'blog_detail.html', {'post': post})
-        return render(request, 'blog.html', {'posts': posts, 'error': f'{slug} is not found.'})
+            if post["slug"] == slug:
+                post["views"] += 1
+                return render(request, "blog_detail.html", {"post": post})
+        return render(
+            request, "blog.html", {"posts": posts, "error": f"{slug} is not found."}
+        )
 
 
 class BlogCreateView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
-        return render(request, 'blog_create.html')
+        return render(request, "blog_create.html")
 
     def post(self, request: HttpRequest) -> HttpResponse:
         post_data = request.POST
 
-        posts.append({
-            'id': 4,
-            'title': post_data.get('title'),
-            'slug': post_data.get('slug'),
-            'reading_minute': post_data.get('reading_minute'),
-            'content': post_data.get('content'),
-            'tg_link': post_data.get('tg_link'),
-            'views': 0,
-        })
+        posts.append(
+            {
+                "id": 4,
+                "title": post_data.get("title"),
+                "slug": post_data.get("slug"),
+                "reading_minute": post_data.get("reading_minute"),
+                "content": post_data.get("content"),
+                "tg_link": post_data.get("tg_link"),
+                "views": 0,
+            }
+        )
 
-        return redirect(reverse('blogs'))
+        return redirect(reverse("blogs"))
+
+
+class BlogDeleteView(View):
+    def get(self, request: HttpRequest, id=None) -> HttpResponse:
+        return render(request, "blog_delete.html", {"blogs": posts})
+
+    def post(self, request: HttpRequest, id: int) -> HttpResponse:
+        global posts
+        posts = [post for post in posts if post["id"] != id]
+        return redirect("blog_delete_list")
+
+
+class BlogUpdateView(View):
+    def get(self, request: HttpRequest, id: int) -> HttpResponse:
+        post = next((p for p in posts if p["id"] == id), None)
+        if not post:
+            return redirect("blogs")
+        return render(request, "blog_update.html", {"post": post})
+
+    def post(self, request: HttpRequest, id: int) -> HttpResponse:
+        post_data = request.POST
+
+        for post in posts:
+            if post["id"] == id:
+                post["title"] = post_data.get("title")
+                post["slug"] = post_data.get("slug")
+                post["reading_minute"] = post_data.get("reading_minute")
+                post["content"] = post_data.get("content")
+                post["tg_link"] = post_data.get("tg_link")
+                break
+
+        return redirect(reverse("blog_detail", kwargs={"slug": post_data.get("slug")}))
